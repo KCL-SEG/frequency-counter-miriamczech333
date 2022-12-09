9adfb357c57d201141838d71b833a2ba2219f5da
Miriam Czech <k21099181@kcl.ac.uk>
2022-12-09 13:56:15 +0000

    solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items: 
+        item_s = str(item)
+        if item_s not in frequencies: 
+            n = 0
+            for i in range(len(items)):
+                if str(items[i]) == item_s:
+                    n = n + 1
+            frequencies[item_s] = n
+
     return frequencies
